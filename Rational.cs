56b8952867fdf902e3a7db31394d02419d04616c
EdwEd <EdwEd>
2020-11-20 16:36:04 +0300

    Was added: Overloads of operators "-","*", "/" Implicit cast from Rational (simple fraction user class) to double Implicit cast  from int to Rational Explicit cast from Rational to int (if it possible with no loss of precision) Some checking for an arithmetic operations with Not-A-Rational numbers values

--- Rational.cs
+++ Rational.cs
@@ -4,19 +4,24 @@ namespace Incapsulation.RationalNumbers
 {
     public class Rational
     {
-        private int numerator;
-        private int denominator;
-        public Rational(int numerator, int denominator=1)
+        public Rational(int numerator, int denominator = 1, bool isReduce = true)
         {
             if (denominator == 0)
             {
                 IsNan = true;
-                throw new DivideByZeroException();
             }
-            if (numerator!=0)
+            if (numerator != 0)
             {
-                Numerator = numerator / BiggestCommonDenominator(numerator, denominator);
-                Denominator = denominator / BiggestCommonDenominator(numerator, denominator);
+                if (!isReduce)
+                {
+                    Numerator = numerator;
+                    Denominator = denominator;
+                }
+                else
+                {
+                    Numerator = FractionReductor(numerator, denominator)[0];
+                    Denominator = FractionReductor(numerator, denominator)[1];
+                }                
             }
             else
             {
@@ -24,7 +29,9 @@ namespace Incapsulation.RationalNumbers
                 Denominator = 1;
             }
         }
-
+        private int numerator;
+        private int denominator;
+       
         public int Numerator
         {
             get { return numerator; }
@@ -45,6 +52,10 @@ namespace Incapsulation.RationalNumbers
 
         public static Rational operator+ (Rational value1,Rational value2)
         {
+            if (value1.IsNan)
+                return value1;
+            if (value2.IsNan)
+                return value2;
             var result = new Rational(1,1);
             if (value1.Denominator == value2.Denominator)
                 result = new Rational(value1.Numerator + value2.Numerator, value2.Denominator);
@@ -52,16 +63,85 @@ namespace Incapsulation.RationalNumbers
             {
                 var arrayEquatedRationals = RationalValuesEquator(value1, value2);
                 result = new Rational(arrayEquatedRationals[0].Numerator + arrayEquatedRationals[1].Numerator, arrayEquatedRationals[0].Denominator);
-            }           
+            }
             return result;
+        }
+
+        public static Rational operator -(Rational value1, Rational value2)
+        {
+            if (value1.IsNan)
+                return value1;
+            if (value2.IsNan)
+                return value2;
+            var result = new Rational(1, 1);
+            if (value1.Denominator == value2.Denominator)
+                result = new Rational(value1.Numerator - value2.Numerator, value1.Denominator);
+            else
+            {
+                var arrayEquatedRationals = RationalValuesEquator(value1, value2);
+                result = new Rational(arrayEquatedRationals[0].Numerator - arrayEquatedRationals[1].Numerator, arrayEquatedRationals[0].Denominator);
+            }
+            return result;
+        }
+
+        public static Rational operator *(Rational value1, Rational value2)
+        {
+            if (value1.IsNan)
+                return value1;
+            if (value2.IsNan)
+                return value2;
+            var result = new Rational(value1.Numerator * value2.Numerator, value1.Denominator * value2.Denominator);
+            return result;
+        }
+
+        public static Rational operator /(Rational value1, Rational value2)
+        {
+            if (value1.IsNan)
+                return value1;
+            if (value2.IsNan)
+                return value2;
+            var result = new Rational(value1.Numerator * value2.Denominator, value1.Denominator * value2.Numerator);
+            return result;
+        }
+
+        public static implicit operator double (Rational value)
+        {
+            if (!value.IsNan)
+                return (double)value.Numerator / value.Denominator;
+            return double.NaN;
+        }
+        public static implicit operator Rational(int value)
+        {
+            return new Rational(value);
+        }
+        public static explicit operator int(Rational value)
+        {
+            if (!value.IsNan)
+            {
+                if (value.Numerator % value.Denominator == 0)
+                {
+                    return value.Numerator / value.Denominator;
+                }
+                else
+                    throw new InvalidCastException();
+            }
+            else
+                throw new ArgumentException();
+        }
+
+
+        //This method converting Rational number to Double value number
+        public double ConvertToDouble()
+        {
+            return 0;
         }
         //This method Equating Rational values to the Lowest Common Denominator
         public static Rational[] RationalValuesEquator (Rational value1, Rational value2)
         {
             int[] orderedDenominators = Ascender(value1.Denominator, value2.Denominator);
             int lowestCommonDen = LowestCommonDenominator(orderedDenominators);
-            var equatedRational1 = NewNumeratorCalculation(value1, lowestCommonDen);
-            var equatedRational2 = NewNumeratorCalculation(value2, lowestCommonDen);
+            var equatedRational1 = NewNumeratorCalculation(value1, lowestCommonDen,false);
+            var equatedRational2 = NewNumeratorCalculation(value2, lowestCommonDen,false);
             return new Rational[] { equatedRational1, equatedRational2 };
         }
         //THis method find Lower Common Denominator for Rational numbers
@@ -95,16 +175,17 @@ namespace Incapsulation.RationalNumbers
             return result;
         }
         //new Enumerator value method based on new denumerator
-        public static Rational NewNumeratorCalculation (Rational oldRational, int newDenominator)
+        public static Rational NewNumeratorCalculation(Rational oldRational, int newDenominator, bool reduce = true)
         {
             int k = newDenominator / oldRational.Denominator;
-            return new Rational(oldRational.Numerator*k,newDenominator);
+            return new Rational(oldRational.Numerator*k,newDenominator,reduce);
         }
         //biggest common Denominator Method
         public static int BiggestCommonDenominator (int number1, int number2)
         {
-            int lNumber = Ascender(number1, number2)[0];
-            int hNumber = Ascender(number1, number2)[1];
+            var arr = Ascender(number1, number2);
+            int lNumber = arr[0];
+            int hNumber = arr[1];
             while(hNumber%lNumber!=0)
             {
                 int tmp = hNumber % lNumber;
@@ -114,10 +195,13 @@ namespace Incapsulation.RationalNumbers
             lNumber = Math.Abs(lNumber);
             return lNumber;
         }
-        public static Rational FractionReductor (Rational value)
+        public static int[] FractionReductor(int numerator, int denominator)
         {
-            int k = BiggestCommonDenominator(value.Numerator, value.Denominator);
-            return new Rational(value.Numerator / k, value.Denominator / k);
+            int k = 1;
+            if (denominator !=0)
+                k = BiggestCommonDenominator(numerator, denominator);
+            return new int[] { numerator / k, denominator / k };
+                           
         }
     }
 }
